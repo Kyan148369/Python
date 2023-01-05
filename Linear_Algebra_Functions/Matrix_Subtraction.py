3aa3afda388a803534e6c8cec22150f8da9e2547
Kyan148369 <kyanssharma10@gmail.com>
2023-01-05 23:59:49 +0530

    Deleted some files and added comments

--- Linear_Algebra_Functions/Matrix_Subtraction.py
+++ Linear_Algebra_Functions/Matrix_Subtraction.py
@@ -5,10 +5,14 @@ cols2 = int(input('Enter number of cols for 2nd matrix to Subtract:'))
 
 matrix1 = inputmatrix(rows, cols)
 matrix2 = inputmatrix(rows2, cols2)
+# checking if its compatible to subtract both the matrices
+
 if ((rows == rows2) and (cols == cols2)):
     def Matrix_Subtraction(matrix1, matrix2):
+        # initalizing subtraction matrix
         matrix_sub = []
         for i in range(rows):
+            # initializing row of the subtraction matrix
             rmsubtraction = []
             for j in range(cols):
                 a = matrix1[i][j]-matrix2[i][j]
